a2fdf45224769bc2bbd5a3209612cac0ff9d39a1
april <aprilashton18@gmail.com>
2022-11-07 04:37:26 -0800

    commit tree c2e6ba9b043922fd78a5f312fd4bb13e8f55bf5f

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
+
+function appendCat(name){
+  let newArr = [...cats, name];
+  return newArr;
+}
+
+function prependCat(name) {
+  let newArr = [name, ...cats];
+  return newArr;
+} 
+
+function removeLastCat() {
+  let newArr = [...cats.slice(0, cats.length-1)];
+  return newArr;
+}
+
+function removeFirstCat() {
+  let newArr = [...cats.slice(1)];
+  return newArr;
+}
